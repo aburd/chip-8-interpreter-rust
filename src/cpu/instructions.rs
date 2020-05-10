d05c69a9c2ae6789614fd1a780ab7e3cc95ae220
aburd <burdick.aaron@gmail.com>
2020-05-10 13:06:21 +0900

    Get rest of instructions

--- src/cpu/instructions.rs
+++ src/cpu/instructions.rs
@@ -24,37 +24,37 @@ pub enum Instruction {
     MostSig(u8), //  8XYE[a]	BitOp 	    Vx<<=1 	Stores the most significant bit of VX in VF and then shifts VX to the left by 1.[b]
     CondNeq(u8, u8), //  9XY0 	Cond 	    if(Vx!=Vy) 	Skips the next instruction if VX doesn't equal VY. (Usually the next instruction is a jump to skip a code block)
     SetI(u16),       //  ANNN 	MEM 	    I = NNN 	Sets I to the address NNN.
-                     //  BNNN 	Flow 	    PC=V0+NNN 	Jumps to the address NNN plus V0.
-                     //  CXNN 	Rand 	    Vx=rand()&NN 	Sets VX to the result of a bitwise and operation on a random number (Typically: 0 to 255) and NN.
-                     //  DXYN 	Disp 	    draw(Vx,Vy,N) 	Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and a height of N pixels. Each row of 8 pixels is read as bit-coded starting from memory location I; I value doesn’t change after the execution of this instruction. As described above, VF is set to 1 if any screen pixels are flipped from set to unset when the sprite is drawn, and to 0 if that doesn’t happen
-                     //  EX9E 	KeyOp 	    if(key()==Vx) 	Skips the next instruction if the key stored in VX is pressed. (Usually the next instruction is a jump to skip a code block)
-                     //  EXA1 	KeyOp 	    if(key()!=Vx) 	Skips the next instruction if the key stored in VX isn't pressed. (Usually the next instruction is a jump to skip a code block)
-                     //  FX07 	Timer 	    Vx = get_delay() 	Sets VX to the value of the delay timer.
-                     //  FX0A 	KeyOp 	    Vx = get_key() 	A key press is awaited, and then stored in VX. (Blocking Operation. All instruction halted until next key event)
-                     //  FX15 	Timer 	    delay_timer(Vx) 	Sets the delay timer to VX.
-                     //  FX18 	Sound 	    sound_timer(Vx) 	Sets the sound timer to VX.
-                     //  FX1E 	MEM 	    I +=Vx 	Adds VX to I. VF is set to 1 when there is a range overflow (I+VX>0xFFF), and to 0 when there isn't.[c]
-                     //  FX29 	MEM 	    I=sprite_addr[Vx] 	Sets I to the location of the sprite for the character in VX. Characters 0-F (in hexadecimal) are represented by a 4x5 font.
-                     //  FX33 	BCD 	    set_BCD(Vx);
-                     //  *(I+0)=BCD(3);
-                     //  *(I+1)=BCD(2);
-                     //  *(I+2)=BCD(1);
-                     //      Stores the binary-coded decimal representation of VX, with the most significant of three digits at the address in I, the middle digit at I plus 1, and the least significant digit at I plus 2. (In other words, take the decimal representation of VX, place the hundreds digit in memory at location in I, the tens digit at location I+1, and the ones digit at location I+2.)
-                     //  FX55 	MEM 	    reg_dump(Vx,&I) 	Stores V0 to VX (including VX) in memory starting at address I. The offset from I is increased by 1 for each value written, but I itself is left unmodified.[d]
-                     //  FX65 	MEM 	    reg_load(Vx,&I) 	Fills V0 to VX (including VX) with values from memory starting at address I. The offset from I is increased by 1 for each value written, but I itself is left unmodified.[d]
-                     // * =========================
-                     // * Super Chip-48 Instructions
-                     // * ==========================
-                     // * - 00Cn - SCD nibble
-                     // * - 00FB - SCR
-                     // * - 00FC - SCL
-                     // * - 00FD - EXIT
-                     // * - 00FE - LOW
-                     // * - 00FF - HIGH
-                     // * - Dxy0 - DRW Vx, Vy, 0
-                     // * - Fx30 - LD HF, Vx
-                     // * - Fx75 - LD R, Vx
-                     // * - Fx85 - LD Vx, R
+    JumpV0NNN(u16),  //  BNNN 	Flow 	    PC=V0+NNN 	Jumps to the address NNN plus V0.
+    RandX(u8, u8), //  CXNN 	Rand 	    Vx=rand()&NN 	Sets VX to the result of a bitwise and operation on a random number (Typically: 0 to 255) and NN.
+    DrawSprite(u8, u8, u8), //  DXYN 	Disp 	    draw(Vx,Vy,N) 	Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and a height of N pixels. Each row of 8 pixels is read as bit-coded starting from memory location I; I value doesn’t change after the execution of this instruction. As described above, VF is set to 1 if any screen pixels are flipped from set to unset when the sprite is drawn, and to 0 if that doesn’t happen
+    KeyPressed(u8), //  EX9E 	KeyOp 	    if(key()==Vx) 	Skips the next instruction if the key stored in VX is pressed. (Usually the next instruction is a jump to skip a code block)
+    KeyUnpressed(u8), //  EXA1 	KeyOp 	    if(key()!=Vx) 	Skips the next instruction if the key stored in VX isn't pressed. (Usually the next instruction is a jump to skip a code block)
+    SetXDelayTimer(u8), //  FX07 	Timer 	    Vx = get_delay() 	Sets VX to the value of the delay timer.
+    AwaitKeyPress(u8), //  FX0A 	KeyOp 	    Vx = get_key() 	A key press is awaited, and then stored in VX. (Blocking Operation. All instruction halted until next key event)
+    SetDelayTimer(u8), //  FX15 	Timer 	    delay_timer(Vx) 	Sets the delay timer to VX.
+    SetSoundTimer(u8), //  FX18 	Sound 	    sound_timer(Vx) 	Sets the sound timer to VX.
+    AddVxToI(u8), //  FX1E 	MEM 	    I +=Vx 	Adds VX to I. VF is set to 1 when there is a range overflow (I+VX>0xFFF), and to 0 when there isn't.[c]
+    SetIWithChar(u8), //  FX29 	MEM 	    I=sprite_addr[Vx] 	Sets I to the location of the sprite for the character in VX. Characters 0-F (in hexadecimal) are represented by a 4x5 font.
+    SetBCD(u8),       //  FX33 	BCD 	    set_BCD(Vx);
+    //  *(I+0)=BCD(3);
+    //  *(I+1)=BCD(2);
+    //  *(I+2)=BCD(1);
+    //      Stores the binary-coded decimal representation of VX, with the most significant of three digits at the address in I, the middle digit at I plus 1, and the least significant digit at I plus 2. (In other words, take the decimal representation of VX, place the hundreds digit in memory at location in I, the tens digit at location I+1, and the ones digit at location I+2.)
+    RegDump(u8), //  FX55 	MEM 	    reg_dump(Vx,&I) 	Stores V0 to VX (including VX) in memory starting at address I. The offset from I is increased by 1 for each value written, but I itself is left unmodified.[d]
+    RegLoad(u8), //  FX65 	MEM 	    reg_load(Vx,&I) 	Fills V0 to VX (including VX) with values from memory starting at address I. The offset from I is increased by 1 for each value written, but I itself is left unmodified.[d]
+                 // * =========================
+                 // * Super Chip-48 Instructions
+                 // * ==========================
+                 // * - 00Cn - SCD nibble
+                 // * - 00FB - SCR
+                 // * - 00FC - SCL
+                 // * - 00FD - EXIT
+                 // * - 00FE - LOW
+                 // * - 00FF - HIGH
+                 // * - Dxy0 - DRW Vx, Vy, 0
+                 // * - Fx30 - LD HF, Vx
+                 // * - Fx75 - LD R, Vx
+                 // * - Fx85 - LD Vx, R
 }
 
 pub fn decode_opcode(opcode: Opcode) -> Result<Instruction, String> {
@@ -66,6 +66,7 @@ pub fn decode_opcode(opcode: Opcode) -> Result<Instruction, String> {
     );
     let nnn = (opcode & 0x0FFF) as u16;
     let nn = (opcode & 0x00FF) as u8;
+    let n = (opcode & 0x000F) as u8;
     let x = ((opcode & 0x0F00) >> 8) as u8;
     let y = ((opcode & 0x00F0) >> 4) as u8;
 
@@ -96,6 +97,26 @@ pub fn decode_opcode(opcode: Opcode) -> Result<Instruction, String> {
         },
         0x9 => Ok(Instruction::CondNeq(x, y)),
         0xA => Ok(Instruction::SetI(nnn)),
+        0xB => Ok(Instruction::JumpV0NNN(nnn)),
+        0xC => Ok(Instruction::RandX(x, nn)),
+        0xD => Ok(Instruction::DrawSprite(x, y, n)),
+        0xE => match nibbles {
+            (_, _, 0x9, 0xE) => Ok(Instruction::KeyPressed(x)),
+            (_, _, 0xA, 0x1) => Ok(Instruction::KeyUnpressed(x)),
+            _ => Err("Opcode not implemented!".to_string()),
+        },
+        0xF => match nibbles {
+            (_, _, 0x0, 0xF) => Ok(Instruction::SetXDelayTimer(x)),
+            (_, _, 0x0, 0xA) => Ok(Instruction::AwaitKeyPress(x)),
+            (_, _, 0x1, 0x5) => Ok(Instruction::SetDelayTimer(x)),
+            (_, _, 0x1, 0x8) => Ok(Instruction::SetSoundTimer(x)),
+            (_, _, 0x1, 0xE) => Ok(Instruction::AddVxToI(x)),
+            (_, _, 0x2, 0x9) => Ok(Instruction::SetIWithChar(x)),
+            (_, _, 0x3, 0x3) => Ok(Instruction::SetBCD(x)),
+            (_, _, 0x5, 0x5) => Ok(Instruction::RegDump(x)),
+            (_, _, 0x6, 0x5) => Ok(Instruction::RegLoad(x)),
+            _ => Err("Opcode not implemented!".to_string()),
+        },
         _ => Err("Opcode not implemented!".to_string()),
     }
 }
